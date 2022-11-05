d6f04c15c6280c4f546c5c65b335c66be337fb44
cjrojasj1 <cj.rojasj1@uniandes.edu.co>
2022-11-05 18:19:15 -0500

    Se arregla fuga de agua: bug.  Imports no usados

--- src/main/java/sakila_api/sakila/StoreRepository.java
+++ src/main/java/sakila_api/sakila/StoreRepository.java
@@ -1,6 +1,5 @@
 package sakila_api.sakila;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Integer> {
